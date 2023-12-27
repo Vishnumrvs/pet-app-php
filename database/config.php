45fd3f783aaf91c986355c54a7187065829c7bec
Vishnumrvs <149359529+Vishnumrvs@users.noreply.github.com>
2023-12-27 15:37:00 +0530

    Update config.php

--- database/config.php
+++ database/config.php
@@ -6,4 +6,3 @@ $host       = getenv('DB_HOST');
 $username   = getenv('DB_USERNAME');
 $password   = getenv('DB_PASSWORD');
 $db_name     = getenv('DB_DATABASE');
-$sslcert    = "ssl/DigiCertGlobalRootCA.crt.pem";
