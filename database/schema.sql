c5130db78b42c6f21da85e07e76459648606bcb1
Vishnumrvs <149359529+Vishnumrvs@users.noreply.github.com>
2023-12-27 15:35:57 +0530

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,23 +1,4 @@
--- Create 'pets' table
-CREATE TABLE `pets` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `owner_id` int(11) DEFAULT NULL,
-  `breed` varchar(255) DEFAULT NULL,
-  `pet_type` varchar(30) DEFAULT NULL,
-  `description` text DEFAULT NULL,
-  `image_path` varchar(255) DEFAULT NULL,
-  `price` decimal(10,2) DEFAULT NULL,
-  `is_available` tinyint(1) DEFAULT 1,
-  PRIMARY KEY (`id`),
-  KEY `owner_id` (`owner_id`),
-  CONSTRAINT `pets_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`)
-);
-
--- Create 'users' table
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-);
+CREATE TABLE Products 
+  ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  ProductName VARCHAR(50) NOT NULL,
+  Price DOUBLE NOT NULL );
